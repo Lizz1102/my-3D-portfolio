304877c092240f720ac2f8b9cfd0f8d5658c0010
Lizz1102 <lizz05110@gmail.com>
2023-12-30 11:59:17 -0500

    Install Tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import React from "react";
-
 function App() {
-    return <div>App</div>;
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline text-red-500">
+                Hello world!
+            </h1>
+        </div>
+    );
 }
 
 export default App;
