570fb3eee623bceca14ee6d481fcd17bf5326222
Lizz1102 <lizz05110@gmail.com>
2023-12-31 22:01:08 -0500

    Add Plane, Bird on homepage

---
+++ src/models/Plane.jsx
@@ -0,0 +1,15 @@
+import { useGLTF, useAnimations } from "@react-three/drei";
+
+import planeScene from "../assets/3d/plane.glb";
+
+const Plane = ({ isRotating, ...props }) => {
+    const { scene, animations } = useGLTF(planeScene);
+
+    return (
+        <mesh>
+            <primitive object={scene} />
+        </mesh>
+    );
+};
+
+export default Plane;
