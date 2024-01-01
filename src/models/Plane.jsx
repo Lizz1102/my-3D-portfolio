e0af77b09d73708660bfe97bcaba449980dc8ee8
Lizz1102 <lizz05110@gmail.com>
2024-01-01 01:41:48 -0500

    Add animation and movements of bird, plane and sky

--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -1,12 +1,23 @@
-import { useGLTF, useAnimations } from "@react-three/drei";
+import { useRef, useEffect } from "react";
 
 import planeScene from "../assets/3d/plane.glb";
+import { useGLTF, useAnimations } from "@react-three/drei";
 
 const Plane = ({ isRotating, ...props }) => {
+    const ref = useRef();
     const { scene, animations } = useGLTF(planeScene);
+    const { actions } = useAnimations(animations, ref);
 
+    useEffect(() => {
+        if (isRotating) {
+            actions["Take 001"].play();
+        } else {
+            actions["Take 001"].stop();
+        }
+    }, [actions, isRotating]);
+    
     return (
-        <mesh {...props}>
+        <mesh {...props} ref={ref}>
             <primitive object={scene} />
         </mesh>
     );
--- src/models/Sky.jsx
+++ src/models/Sky.jsx
@@ -8,11 +8,17 @@ const Sky = ({ isRotating }) => {
     const sky = useGLTF(skyScene);
     const skyRef = useRef();
 
+    useFrame((_, delta) => {
+        if (isRotating) {
+            skyRef.current.rotation.y += 0.15 * delta;
+        }
+    });
+
     return (
         <mesh ref={skyRef}>
             <primitive object={sky.scene} />
         </mesh>
     );
-}
+};
 
 export default Sky;
