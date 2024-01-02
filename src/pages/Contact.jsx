b140f2410be2f39c829ecb33ec50a61060f07cbe
Lizz1102 <lizz05110@gmail.com>
2024-01-02 00:35:44 -0500

    Add Fox animations on contact page, add alerts for emailing status

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,27 +2,31 @@ import emailjs from "@emailjs/browser";
 import { Canvas } from "@react-three/fiber";
 import { Suspense, useRef, useState } from "react";
 
-//import { Fox } from "../models";
-//import useAlert from "../hooks/useAlert";
-//import { Alert, Loader } from "../components";
+import Fox from "../models/Fox";
+import useAlert from "../hooks/useAlert";
+import Loader from "../components/Loader";
+import Alert from "../components/Alert";
 
 const Contact = () => {
     const formRef = useRef(null);
     const [form, setForm] = useState({ name: "", email: "", message: "" });
     //const { alert, showAlert, hideAlert } = useAlert();
     const [isLoading, setIsLoading] = useState(false);
-    //const [currentAnimation, setCurrentAnimation] = useState("idle");
+    const [currentAnimation, setCurrentAnimation] = useState("idle");
+
+    const { alert, showAlert, hideAlert } = useAlert();
 
     const handleChange = (e) => {
         setForm({ ...form, [e.target.name]: e.target.value });
     };
 
-    const handleFocus = () => {};
-    const handleBlur = () => {};
+    const handleFocus = () => setCurrentAnimation("walk");
+    const handleBlur = () => setCurrentAnimation("idle");
 
     const handleSubmit = (e) => {
         e.preventDefault();
         setIsLoading(true);
+        setCurrentAnimation("hit");
         emailjs
             .send(
                 import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
@@ -38,19 +42,36 @@ const Contact = () => {
             )
             .then((result) => {
                 console.log(result.text);
-                //showAlert("Message sent successfully!", "success");
+
                 setIsLoading(false);
-                setForm({ name: "", email: "", message: "" });
+                showAlert({
+                    show: true,
+                    text: "Message sent successfully!",
+                    type: "success",
+                });
+
+                setTimeout(() => {
+                    hideAlert();
+                    setCurrentAnimation("idle");
+                    setForm({ name: "", email: "", message: "" });
+                }, [3000]);
             })
             .catch((error) => {
-                console.log(error.text);
-                //showAlert("Something went wrong!", "error");
                 setIsLoading(false);
+                setCurrentAnimation("idle");
+                console.log(error.text);
+                showAlert({
+                    show: true,
+                    text: "Failed to send message!",
+                    type: "danger",
+                });
             });
     };
 
     return (
         <section className="relative flex lg:flex-row flex-col max-container">
+            {alert.show && <Alert {...alert} />}
+
             <div className="flex-1 min-w-[50%] flex flex-col">
                 <h1 className="head-text">Get in Touch</h1>
 
@@ -111,6 +132,28 @@ const Contact = () => {
                         {isLoading ? "Sending..." : "Send Message"}
                     </button>
                 </form>
+            </div>
+
+            <div className="lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]">
+                <Canvas
+                    camera={{
+                        position: [0, 0, 5],
+                        fov: 75,
+                        near: 0.1,
+                        far: 1000,
+                    }}
+                >
+                    <directionalLight intensity={2.5} position={[0, 0, 1]} />
+                    <ambientLight intensity={0.5} />
+                    <Suspense fallback={<Loader />}>
+                        <Fox
+                            currentAnimation={currentAnimation}
+                            position={[0.5, 0.35, 0]}
+                            rotation={[12.6, -0.6, 0]}
+                            scale={[0.5, 0.5, 0.5]}
+                        />
+                    </Suspense>
+                </Canvas>
             </div>
         </section>
     );
