3f1fb914a470b0191b4cfa120688380299f0fe4f
Lizz1102 <lizz05110@gmail.com>
2024-01-05 16:04:14 -0500

    Show percentage loaded in loaders

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,10 +1,15 @@
-import { Html } from "@react-three/drei";
+import { Html, useProgress } from "@react-three/drei";
 
 const Loader = () => {
+    const { progress } = useProgress();
     return (
         <Html>
             <div className="flex justify-center items-center">
-                <div className="w-20 h-20 border-2 border-opacity-20 border-blue-500 border-t-blue-500 rounded-full animate-spin"></div>
+                <div className="w-20 h-20 border-2 border-opacity-20 border-blue-500 border-t-blue-500 rounded-full animate-spin relative">
+                    <div className="absolute inset-0 flex items-center justify-center text-blue-500">
+                        {Math.round(progress)}%
+                    </div>
+                </div>
             </div>
         </Html>
     );
